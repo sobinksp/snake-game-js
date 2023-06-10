82e7fa01b7b8591b190ca0f0e68db7445d354c4e
Sobin <sobingeneral@gmail.com>
2023-06-10 23:00:52 +0700

    make player be able to move pass the border

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let targetX, targetY;
 let playerX = 10,
-  playerY = 10;
+  playerY = 10,
+  playerBody = [];
 let velocityX = 0,
   velocityY = 0;
 function targetPositionGenerator() {
@@ -31,14 +32,47 @@ function initGame() {
   const boardGrid = document.querySelector(".board");
   let htmlMarkup;
   htmlMarkup = `<div class ="target" style="grid-area: ${targetY}/ ${targetX}"></div>`;
+  if (playerX === targetX && playerY === targetY) {
+    targetPositionGenerator();
+    playerBody.push([targetX, targetY]);
+    // console.log("after eat", playerBody);
+  }
+  // shifting player body value
+  for (let i = playerBody.length - 1; i > 0; i--) {
+    playerBody[i] = playerBody[i - 1];
+    // console.log("shifting values", playerBody);
+  }
+
+  // player head = current position
+  playerBody[0] = [playerX, playerY];
   playerX += velocityX;
   playerY += velocityY;
-  htmlMarkup += `<div class ="player" style="grid-area: ${playerY}/ ${playerX}"></div>`;
+  if (playerBody[0][0] < 1) {
+    playerX = 30;
+    playerBody[0] = [playerX, playerY];
+  } else if (playerBody[0][0] > 30) {
+    playerX = 1;
+    playerBody[0] = [playerX, playerY];
+  } else if (playerBody[0][1] < 1) {
+    playerY = 30;
+    playerBody[0] = [playerX, playerY];
+  } else if (playerBody[0][1] > 30) {
+    playerY = 1;
+    playerBody[0] = [playerX, playerY];
+  }
+  //   console.log(playerBody);
+  for (let i = 0; i < playerBody.length; i++) {
+    // console.log(playerBody);
+    htmlMarkup += `<div class ="player" style="grid-area: ${playerBody[i][1]}/ ${playerBody[i][0]}"></div>`;
+    // console.log(htmlMarkup);
+    // htmlMarkup += `<div class ="player" style="grid-area: ${playerY}/ ${playerX}"></div>`;
+  }
   boardGrid.innerHTML = htmlMarkup;
 }
 
 targetPositionGenerator();
-initGame();
+// initGame();
+setInterval(initGame, 150);
 
 // the first parameter is event which can be found on the internet.
 // the second parameter is function name.

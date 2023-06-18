9d8ab951b2da4ecf5cacf79b2158557a4606bd7e
Sobin <sobingeneral@gmail.com>
2023-06-18 23:52:06 +0700

    trying to fix border collision issue
    playerbody[0] and playerbody[1] got the same value when hit the border

--- script.js
+++ script.js
@@ -31,17 +31,17 @@ function playerMovement(e) {
 
 function borderCollision() {
   if (playerBody[0][0] < 1) {
-    playerX = 30;
-    playerBody[0] = [playerX, playerY];
+    // playerX = 30;
+    playerBody[0] = [30, playerY];
   } else if (playerBody[0][0] > 30) {
-    playerX = 1;
-    playerBody[0] = [playerX, playerY];
+    // playerX = 1;
+    playerBody[0] = [1, playerY];
   } else if (playerBody[0][1] < 1) {
-    playerY = 30;
-    playerBody[0] = [playerX, playerY];
+    // playerY = 30;
+    playerBody[0] = [playerX, 30];
   } else if (playerBody[0][1] > 30) {
-    playerY = 1;
-    playerBody[0] = [playerX, playerY];
+    // playerY = 1;
+    playerBody[0] = [playerX, 1];
   }
 }
 function pause() {
@@ -67,25 +67,38 @@ function initGame() {
   const boardGrid = document.querySelector(".board");
   let htmlMarkup;
   htmlMarkup = `<div class ="target" style="grid-area: ${targetY}/ ${targetX}"></div>`;
+
   if (playerX === targetX && playerY === targetY) {
     targetPositionGenerator();
     playerBody.push([targetX, targetY]);
   }
+  //   borderCollision(); // check if player head hit the border
   // shifting player body value
   for (let i = playerBody.length - 1; i > 0; i--) {
     playerBody[i] = playerBody[i - 1];
   }
-
+  //   console.log("shifting body", playerBody);
   playerBody[0] = [playerX, playerY]; // player head is current position
   playerX += velocityX;
   playerY += velocityY;
 
   borderCollision(); // check if player head hit the border
+
   //   playerBody[x][1] = column
   //   playerBody[x][0] = row
   for (let i = 0; i < playerBody.length; i++) {
     htmlMarkup += `<div class ="player" style="grid-area: ${playerBody[i][1]}/ ${playerBody[i][0]}"></div>`;
-    // if ()
+    if (
+      i !== 0 &&
+      playerBody[0][1] === playerBody[i][1] &&
+      playerBody[0][0] === playerBody[i][0]
+    ) {
+      console.log("GAME OVER");
+      console.log("this is head", playerBody[0][0], playerBody[0][1]);
+      console.log("this is body", playerBody[i][0], playerBody[i][1]);
+      console.log("this is whole body", playerBody);
+      gameOver();
+    }
   }
   boardGrid.innerHTML = htmlMarkup;
 }
